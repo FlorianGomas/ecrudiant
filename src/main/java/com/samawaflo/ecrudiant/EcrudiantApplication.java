64557cda849be659e74e5e950c8d236ef2e7fb41
fgomas <florian.gomas@etu.u-bordeaux.fr>
2022-05-16 08:17:36 +0200

    first commit

---
+++ src/main/java/com/samawaflo/ecrudiant/EcrudiantApplication.java
@@ -0,0 +1,13 @@
+package com.samawaflo.ecrudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcrudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcrudiantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samawaflo/ecrudiant/EcrudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samawaflo.ecrudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcrudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
