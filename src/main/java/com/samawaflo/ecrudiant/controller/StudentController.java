a0743fe29d1a24a65a7513452b7cdf78d5c394c2
fgomas <florian.gomas@etu.u-bordeaux.fr>
2022-05-18 12:45:17 +0200

    Ajout des cross origins

--- src/main/java/com/samawaflo/ecrudiant/controller/StudentController.java
+++ src/main/java/com/samawaflo/ecrudiant/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.samawaflo.ecrudiant.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,28 +23,33 @@ public class StudentController {
 	@Autowired
 	private StudentService studentService;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(value="/ecrudiant/rest/students")
 	public List<Student> loadAllStudents(){
 		return studentService.findAll();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(value="/ecrudiant/rest/students/{id}")
 	public Student loadStudent(@PathVariable("id") long id){
 		return studentService.findStudent(id);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping(value="/ecrudiant/rest/students")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Student newStudent(@RequestBody Student newStudent){
 		return studentService.saveStudent(newStudent);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PutMapping(value="/ecrudiant/rest/students/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Student updateStudent(@RequestBody Student newStudent,  @PathVariable Long id){
 		return studentService.replaceStudent(newStudent,id);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@DeleteMapping(value="/ecrudiant/rest/students/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteStudent(@PathVariable Long id){
